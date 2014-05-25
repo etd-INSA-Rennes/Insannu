ca1529312571f0376a56312ed551801d100ea621
Paul Chaignon <paul.chaignon@gmail.com>
2014-05-25 11:03:23 +0200

    Contact info changed

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,6 @@
 <div id="footer">
 	<p>Merci de signaler tout bug <a href="mailto:pchaigno@insa-rennes.fr">en nous envoyant un e-mail</a>.</p>
-	<a href="/insannu">Contact</a>
+	<a href="mailto:pchaigno@insa-rennes.fr">Contact</a>
 	<img src="/img/star.gif" alt="Separation mark"/>
 	<a href="/about.php">&Agrave; propos</a>
 	<img src="/img/star.gif" alt="Separation mark"/>
