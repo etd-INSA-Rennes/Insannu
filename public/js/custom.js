d5747242032193eb7f88f01904eeb096f880184f
Quentin Dufour <quentin@dufour.tk>
2014-09-04 15:42:26 +0200

    Loading cat

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,13 +1,16 @@
 angular.module('insannu', ['ngResource'])
   .controller('SearchController', ['$scope', '$resource', function($scope, $resource) {
     var StudentsFactory = $resource('/api.php/search/:search');
+    $scope.loaded = false;
 
     $scope.$watch('search', function() {
       $scope.listEmail = "";
+      $scope.loaded = false;
       $scope.students = StudentsFactory.query({search:$scope.search}, function() {
         $scope.students.forEach(function(obj) {
           $scope.listEmail += obj.mail + ", ";
         });
+        $scope.loaded = true;
         console.log('Found');
       });
     });
