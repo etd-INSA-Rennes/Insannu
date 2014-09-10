a74f08bd459157892cc32557449478bc4c0e2d92
Quentin Dufour <quentin@dufour.tk>
2014-09-10 17:56:20 +0200

    Adding promo choice

--- connector/Facebook.php
+++ connector/Facebook.php
@@ -19,6 +19,17 @@ class Facebook {
   public static function retrieveGroupMembers() {
     $access_token = file_get_contents(__DIR__.'/access_token.txt', true);
     $url = "https://graph.facebook.com/444451508952924/members?access_token=".$access_token;
-    $groupMembers = file_get_contents($url);
+    $groupMembers = http_get($url);
+    var_dump($groupMembers);
+  }
+
+  private static function http_get($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //One of the worst idea I agree
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);    
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
   }
 }
